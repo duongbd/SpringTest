b78eaabe75750904d194f957bedda3f35c3ce6c3
duongbd <blackdiamond6129@gmail.com>
2021-07-15 09:00:25 +0700

    implement code

--- src/main/java/com/demo/HelloController.java
+++ src/main/java/com/demo/HelloController.java
@@ -1,18 +1,21 @@
 package com.demo;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("")
 @Controller
+@RequestMapping("/hello")
 public class HelloController {
-    @RequestMapping(method = RequestMethod.GET)
-    public String printHello(Model modelMap) {
-        String s = "ABC";
-        modelMap.addAttribute("inbox", s);
+    @GetMapping ("/{number}")
+    public String printHello(Model modelMap,@RequestParam Integer number) {
+        modelMap.addAttribute("inbox", number*=23000);
+        return "hello";
+    }
+    @GetMapping()
+    public String main(Model modelMap) {
         return "hello";
     }
 }
--- src/main/webapp/WEB-INF/hello.jsp
+++ src/main/webapp/WEB-INF/hello.jsp
@@ -5,12 +5,18 @@
   Time: 2:14 PM
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="charset=UTF-8"  %>
+<%@ page contentType="charset=UTF-8" %>
 <html>
-<head>
-    <title>Title</title>
-</head>
-<body>
-<h2>${inbox}</h2>
-</body>
+    <head>
+        <title>Title</title>
+    </head>
+    <body>
+        <form name="testForm" action="/hello/?number={number}">
+            <br/>
+            <input type="int" name="number"/>
+            <br/>
+            <input type="submit"/>
+        </form>
+        <h2>${inbox}</h2>
+    </body>
 </html>
